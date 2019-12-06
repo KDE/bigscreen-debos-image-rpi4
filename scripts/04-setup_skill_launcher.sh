3678a6b3b24a4b56bc35fc63eb22bd67464a64cd
Marco Martin <notmart@gmail.com>
2019-12-06 11:18:14 +0100

    this is installed by the package

--- scripts/04-setup_skill_launcher.sh
+++
@@ -1,4 +0,0 @@
-# Get skill_launcher
-wget -O /usr/bin/mycroft-skill-launcher.py https://raw.githubusercontent.com/AIIX/gui-skills/master/mycroft-skill-launcher.py
-# Set permissions
-chmod a+x /usr/bin/mycroft-skill-launcher.py
