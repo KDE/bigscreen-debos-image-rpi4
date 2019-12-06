3b4d436fbc24ef5e78c5475fee6c92ce93632009
Marco Martin <notmart@gmail.com>
2019-12-06 16:55:54 +0100

    copy autostart

--- scripts/03-setup_cec.sh
+++ scripts/03-setup_cec.sh
@@ -2,9 +2,15 @@
 
 USER=mycroft
 GROUP=uinput
+UGID=32011
 
 pip3 install python-uinput
 
 groupadd ${GROUP}
 
 usermod -a -G ${GROUP} ${USER}
+
+cp -r /etc/skel/.config/* /home/$USER/.config/
+
+chown -R $UGID:$UGID /opt/mycroft
+chown -R $UGID:$UGID /home/$USER
