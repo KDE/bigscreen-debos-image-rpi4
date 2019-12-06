870e996876ed64a25f749d38780a2de5950cf6c1
Marco Martin <notmart@gmail.com>
2019-12-06 13:45:15 +0100

    install desktop files in there

--- scripts/93-prepare-bigscreen-skills.sh
+++ scripts/93-prepare-bigscreen-skills.sh
@@ -4,8 +4,9 @@ USER=mycroft
 # Unpack mycroft-skills-aarch64.tar.gz to user home
 tar -xzf /var/tmp/mycroft-skills-aarch64.tar.gz -C /home/$USER/
 rm /var/tmp/mycroft-skills-aarch64.tar.gz
-
+cp -r /etc/skel/.local/* /home/$USER/.local/
 
 chown -R $UGID:$UGID /opt/mycroft
+chown -R $UGID:$UGID /home/$USER
 
 
