1f2ff46f98900975863b0b1bac0f01db2d4bf5cd
Marco Martin <notmart@gmail.com>
2019-12-10 21:42:06 +0100

    add an overlay of ~/.mycroft

--- scripts/93-prepare-bigscreen-skills.sh
+++ scripts/93-prepare-bigscreen-skills.sh
@@ -6,6 +6,8 @@ tar -xzf /var/tmp/mycroft-skills-aarch64.tar.gz -C /
 rm /var/tmp/mycroft-skills-aarch64.tar.gz
 cp -r /etc/skel/.local/* /home/$USER/.local/
 
+tar -xzf /var/tmp/dotmycroft-setup-aarch64.tar.gz -C /home/mycroft
+
 chown -R $UGID:$UGID /opt/mycroft
 chown -R $UGID:$UGID /home/$USER
 
