2e7f6884da285f2df8f3fbb53a2689e951d2648e
Aditya Mehra <Aix.m@outlook.com>
2019-12-24 01:09:59 +1030

    add gltron configrc

--- scripts/93-prepare-bigscreen-skills.sh
+++ scripts/93-prepare-bigscreen-skills.sh
@@ -7,6 +7,7 @@ rm /var/tmp/mycroft-skills-aarch64.tar.gz
 cp -r /etc/skel/.local/* /home/$USER/.local/
 cp -r /etc/xdg/kglobalshortcutsrc /home/$USER/.config/
 cp -r /etc/xdg/applications-blacklistrc /home/$USER/.config/
+cp -r /etc/skel/.local/.gltronrc /home/$USER/
 
 tar -xzf /var/tmp/dotmycroft-setup-aarch64.tar.gz -C /home/mycroft
 
