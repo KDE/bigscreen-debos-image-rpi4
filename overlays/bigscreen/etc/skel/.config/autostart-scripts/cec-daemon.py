fb07abaf2a56e0ec29d5d628a522dddbcc2169a8
Aditya Mehra <Aix.m@outlook.com>
2020-01-17 19:10:16 +1030

    improve key navigation with cec

--- overlays/bigscreen/etc/skel/.config/autostart-scripts/cec-daemon.py
+++ overlays/bigscreen/etc/skel/.config/autostart-scripts/cec-daemon.py
@@ -30,13 +30,27 @@ cec.init()
 device = uinput.Device(KEYMAP.values())
 
 print("Ready")
+keystate = None
 
 def onkey(event, key, state):
-  if state > 0:
-    print("Got key", key, "state", state)
-    device.emit_click(KEYMAP[key])
+    assert event == 2
+    global keystate
+    if state == 0 and keystate is None:
+        print("Got Key", key, "state", state)
+        keystate = "down"
+        device.emit(KEYMAP[key], 1)
+
+    if state > 0:
+        if keystate is None:
+           print("Got key", key, "state", state)
+           device.emit(KEYMAP[key], 1)
+
+        print("Key {0} up after {1}".format(key, state))
+        device.emit(KEYMAP[key], 0)
+        keystate = None
 
 cec.add_callback(onkey, cec.EVENT_KEYPRESS)
 
 while True:
  time.sleep(9e9)
+ 
