5c02ea1a9bb43965e4ee04ec0198af68f949700e
Aditya Mehra <Aix.m@outlook.com>
2020-03-11 22:28:58 +1030

    try fix autoResolution script perms

--- scripts/96-prepare-auto-resolution.sh
+++ scripts/96-prepare-auto-resolution.sh
@@ -1,5 +1,4 @@
-#!/bin/sh -ex
-
+UGID=32011
 USER=mycroft
 
-cp -r /etc/skel/.config/autostart-scripts/autoResolution.py /home/$USER/.config/autostart-scripts/ 
+cp -r /etc/skel/.config/autostart-scripts/autoResolution.py /home/$USER/.config/autostart-scripts/
