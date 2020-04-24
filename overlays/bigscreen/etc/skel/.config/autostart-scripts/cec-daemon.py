1395d2483f609c5367bd24a47b7aff3339d9d31a
Aditya Mehra <Aix.m@outlook.com>
2020-04-24 16:42:19 +0530

    Add seperated cec-daemon and keymap configuration

--- overlays/bigscreen/etc/skel/.config/autostart-scripts/cec-daemon.py
+++ overlays/bigscreen/etc/skel/.config/autostart-scripts/cec-daemon.py
@@ -3,34 +3,23 @@
 import cec
 import uinput
 import time
+from configparser import ConfigParser
 
 u = uinput
+config = ConfigParser()
+keymap = []
+config.read('keymap.ini')
+keyconfig = config._sections['default-keymap']
 
-KEYMAP = {
-   0: u.KEY_ENTER,
-   1: u.KEY_UP,
-   2: u.KEY_DOWN,
-   3: u.KEY_LEFT,
-   4: u.KEY_RIGHT,
-   9: u.KEY_HOMEPAGE,
-  10: u.KEY_MENU,
-  13: u.KEY_BACK,
-  68: u.KEY_PLAY,
-  69: u.KEY_STOP,
-  70: u.KEY_PAUSE,
-  75: u.KEY_FASTFORWARD,
-  76: u.KEY_REWIND,
- 113: u.KEY_BLUE,
- 114: u.KEY_RED,
- 115: u.KEY_YELLOW,
- 116: u.KEY_GREEN
-}
+for key, value in keyconfig.items():
+    keymap.append(getattr(uinput, value))
 
 cec.init()
-device = uinput.Device(KEYMAP.values())
+device = uinput.Device(keymap)
 
 print("Ready")
 keystate = None
+
 
 def onkey(event, key, state):
     assert event == 2
@@ -42,15 +31,15 @@ def onkey(event, key, state):
 
     if state > 0:
         if keystate is None:
-           print("Got key", key, "state", state)
-           device.emit(KEYMAP[key], 1)
+            print("Got key", key, "state", state)
+            device.emit(KEYMAP[key], 1)
 
         print("Key {0} up after {1}".format(key, state))
         device.emit(KEYMAP[key], 0)
         keystate = None
 
+
 cec.add_callback(onkey, cec.EVENT_KEYPRESS)
 
 while True:
- time.sleep(9e9)
- 
+    time.sleep(9e9)
