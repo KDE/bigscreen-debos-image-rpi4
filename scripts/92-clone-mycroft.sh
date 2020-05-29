bd7c339ea70d19a05af01dbc57310fd75fac7409
Aditya Mehra <Aix.m@outlook.com>
2020-05-29 16:43:39 +0530

    use architecture variable instead of hardcoding

--- scripts/92-clone-mycroft.sh
+++ scripts/92-clone-mycroft.sh
@@ -1,9 +1,9 @@
 UGID=32011
 USER=mycroft
 
-# Unpack mycroft-core-setup-aarch64.tar.gz to user home
-tar -xzf /var/tmp/mycroft-core-setup-aarch64.tar.gz -C /home/$USER/
-rm /var/tmp/mycroft-core-setup-aarch64.tar.gz
+# Unpack mycroft-core-setup-{{arch}}.tar.gz to user home
+tar -xzf /var/tmp/mycroft-core-setup-$1.tar.gz -C /home/$USER/
+rm /var/tmp/mycroft-core-setup-$1.tar.gz
 
 mkdir -p /opt/mycroft/skills
 mkdir -p /var/log/mycroft
@@ -19,5 +19,8 @@ chown -R $UGID:$UGID /opt/mycroft
 chown -R $UGID:$UGID /var/log/mycroft
 
 
-# Install mimic
-dpkg --install /var/tmp/mimic-arm64_1.2.0.2+1559651054-1.deb
+if [ $1 == "armhf" ]; then
+    dpkg --install /var/tmp/mimic_1.3.0.1_armhf.deb
+else
+    dpkg --install /var/tmp/mimic-arm64_1.2.0.2+1559651054-1.deb
+fi
--- scripts/93-prepare-bigscreen-skills.sh
+++ scripts/93-prepare-bigscreen-skills.sh
@@ -1,15 +1,15 @@
 UGID=32011
 USER=mycroft
 
-# Unpack mycroft-skills-aarch64.tar.gz to user home
-tar -xzf /var/tmp/mycroft-skills-aarch64.tar.gz -C /
-rm /var/tmp/mycroft-skills-aarch64.tar.gz
+# Unpack mycroft-skills-{{arch}}.tar.gz to user home
+tar -xzf /var/tmp/mycroft-skills-$1.tar.gz -C /
+rm /var/tmp/mycroft-skills-$1.tar.gz
 cp -r /etc/skel/.local/* /home/$USER/.local/
 cp -r /etc/xdg/kglobalshortcutsrc /home/$USER/.config/
 cp -r /etc/xdg/applications-blacklistrc /home/$USER/.config/
 cp -r /etc/skel/.gltronrc /home/$USER/
 
-tar -xzf /var/tmp/dotmycroft-setup-aarch64.tar.gz -C /home/mycroft
+tar -xzf /var/tmp/dotmycroft-setup-$1.tar.gz -C /home/mycroft
 
 chown -R $UGID:$UGID /opt/mycroft
 chown -R $UGID:$UGID /home/$USER
