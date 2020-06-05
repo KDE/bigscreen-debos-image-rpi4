dcee3912e956f8de8ad13408b7abadf11eaca216
Bhushan Shah <bhush94@gmail.com>
2020-06-05 13:24:19 +0530

    when on armhf simply copy vmlinuz

--- scripts/01-pi_copy_kernel.sh
+++ scripts/01-pi_copy_kernel.sh
@@ -2,5 +2,9 @@
 
 cd /boot
 
-cp /boot/Image.gz-* /boot/firmware/kernel8.img.gz
-gunzip /boot/firmware/kernel8.img.gz
+if [ $1 = "armhf" ]; then
+    cp /boot/vmlinuz-* /boot/firmware/kernel7.img
+else
+    cp /boot/Image.gz-* /boot/firmware/kernel8.img.gz
+    gunzip /boot/firmware/kernel8.img.gz
+fi
