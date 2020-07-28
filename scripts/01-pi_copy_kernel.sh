757db09c8738b88d59e5d7b35b9939e91a7a31d7
Bhushan Shah <bhush94@gmail.com>
2020-07-28 14:58:40 +0530

    proper file name

--- scripts/01-pi_copy_kernel.sh
+++ scripts/01-pi_copy_kernel.sh
@@ -5,6 +5,6 @@ cd /boot
 if [ $1 = "armhf" ]; then
     cp /boot/vmlinuz-* /boot/firmware/kernel.img
 else
-    cp /boot/Image.gz-* /boot/firmware/kernel8.img.gz
+    cp /boot/vmlinuz-* /boot/firmware/kernel8.img.gz
     gunzip /boot/firmware/kernel8.img.gz
 fi
