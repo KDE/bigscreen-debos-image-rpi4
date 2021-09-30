5fdfac6f95d953f8b0b3e3e52414b98cddbead8d
Aditya Mehra <aix.m@outlook.com>
2021-09-30 19:26:50 +0930

    setup different mycroft configs for arm64 and armhf

---
+++ scripts/100-mycroft-conf-arm64.sh
@@ -0,0 +1,5 @@
+UGID=32011
+USER=mycroft
+
+mv /etc/mycroft/mycroft.arm64 /etc/mycroft/mycroft.conf
+rm /etc/mycroft/mycroft.armhf
---
+++ scripts/100-mycroft-conf-armhf.sh
@@ -0,0 +1,5 @@
+UGID=32011
+USER=mycroft
+
+mv /etc/mycroft/mycroft.armhf /etc/mycroft/mycroft.conf
+rm /etc/mycroft/mycroft.arm64
